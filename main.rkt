c7fa65aff2b5d4066ed7c65a39750b60c6813309
Jay McCarthy <jay.mccarthy@gmail.com>
2018-03-07 09:49:42 -0500

    ttt

--- main.rkt
+++ main.rkt
@@ -1,5 +1,6 @@
 #lang racket/base
-(require racket/match)
+(require racket/flonum
+         racket/match)
 
 (define-syntax-rule (while test . body)
   (let loop () (when test (begin . body) (loop))))
@@ -48,10 +49,10 @@
 
 (define tic-tac-toe-interactive
   (λ (who-am-i)
+    (local-require raart)
+    (define label-table
+      (table (text-rows '((0 1 2) (3 4 5) (6 7 8)))))
     (λ (board)
-      (local-require raart)
-      (define label-table
-        (table (text-rows '((0 1 2) (3 4 5) (6 7 8)))))
       (define (display-board)
         (define (draw-row start)
           (for/list ([i (in-range start (+ start 3))])
@@ -75,6 +76,46 @@
         (match (read)
           [(? number? n) (cons who-am-i n)]
           [_ (loop)])))))
+
+(define (vecmat-mult! m p A B C)
+  (unless (= (flvector-length A) m)
+    (error 'vecmat-multiply "A is wrong size"))
+  (unless (= (flvector-length C) p)
+    (error 'vecmat-multiply "A is wrong size"))
+  (for ([j (in-range p)])
+    (flvector-set!
+     C j
+     (for/sum ([a (in-flvector A)]
+               [b (in-flvector B (fx* j m))])
+       (fl* a b)))))
+
+(define (tic-tac-toe-ai ai-matrix)
+  (define input-vec (make-flvector 16))
+  (define output-vec (make-flvector 8))
+  (λ (who-am-i)
+    (λ (board)
+      ;; 0-8 are "spot is open"
+      (for ([i (in-range 0 9)]
+            [x (in-vector board)])
+        (flvector-set! input-vec i (if (not x) 1.0 0.0)))
+      ;; 9-17 are "spot is me"
+      (for ([i (in-range 0 9)]
+            [x (in-vector board)])
+        (flvector-set! input-vec (+ 9 i) (if (eq? x who-am-i) 1.0 0.0)))
+
+      ;; A     =  1 x 16
+      ;;     B = 16 x 8
+      ;; A * B =  1 x 8
+      (vecmat-mult! 16 8 input-vec ai-matrix output-vec)
+
+      (for/fold ([spot #f]
+                 [spot-score 0]
+                 #:return (cons who-am-i spot))
+                ([i (in-naturals)]
+                 [x (in-flvector output-vec)])
+        (if (< spot-score x)
+          (values i x)
+          (values spot spot-score))))))
 
 (module+ test
   (tic-tac-toe tic-tac-toe-interactive
