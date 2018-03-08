3a6a6004fcfb8a1c7af41f623afa932920cd1516
Jay McCarthy <jay.mccarthy@gmail.com>
2018-03-07 22:13:19 -0500

    up

--- main.rkt
+++ main.rkt
@@ -2,7 +2,9 @@
 (require racket/flonum
          racket/fixnum
          racket/list
-         racket/match)
+         racket/match
+         data/heap
+         raart)
 
 (define-syntax-rule (while test . body)
   (let loop () (when test (begin . body) (loop))))
@@ -13,7 +15,7 @@
     (set! y x)
     (set! x tmp)))
 
-(define (tic-tac-toe player1 player2)
+(define (tic-tac-toe player1 player2 #:display? [display? #f])
   (define board (make-vector 9 #f))
   (define (same? x y z)
     (and (eq? x y) (eq? y z) x))
@@ -41,37 +43,38 @@
 
   (define active-player (player1 'O))
   (define inactive-player (player2 'X))
+  (when display? (display-board board))
   (until (game-complete?)
     (define player-move #f)
     (until (and player-move (valid-move? player-move))
       (set! player-move (active-player board)))
     (evaluate-move! player-move)
+    (when display? (display-board board))
     (swap! active-player inactive-player))
   (game-complete?))
 
+(define label-table
+  (table (text-rows '((0 1 2) (3 4 5) (6 7 8)))))
+(define (display-board board)
+  (define (draw-row start)
+    (for/list ([i (in-range start (+ start 3))])
+      (char (match (vector-ref board i)
+              [#f #\space]
+              ['O #\O]
+              ['X #\X]))))
+  (draw-here
+   (happend
+    (table
+     (list (draw-row 0)
+           (draw-row 3)
+           (draw-row 6)))
+    (blank 3 7)
+    label-table)))
+
 (define tic-tac-toe-interactive
   (λ (who-am-i)
-    (local-require raart)
-    (define label-table
-      (table (text-rows '((0 1 2) (3 4 5) (6 7 8)))))
     (λ (board)
-      (define (display-board)
-        (define (draw-row start)
-          (for/list ([i (in-range start (+ start 3))])
-            (char (match (vector-ref board i)
-                    [#f #\space]
-                    ['O #\O]
-                    ['X #\X]))))
-        (draw-here
-         (happend
-          (table
-           (list (draw-row 0)
-                 (draw-row 3)
-                 (draw-row 6)))
-          (blank 3 7)
-          label-table)))
-
-      (display-board)
+      (display-board board)
       (let loop ()
         (printf "~a: " who-am-i)
         (flush-output)
@@ -79,7 +82,119 @@
           [(? number? n) (cons who-am-i n)]
           [_ (loop)])))))
 
-;; http://www.ijcte.org/papers/799-T098.pdf
+;; Tree-based AI
+
+;; Language inspired by --- http://www.ijcte.org/papers/799-T098.pdf
+;; --- It has other ideas about how to run the tournament efficiently
+;; (essentially sample the opponents rather than play everyone)
+
+;; This paper ---
+;; https://www.jair.org/media/1338/live-1338-2278-jair.pdf --- could
+;; be interesting to implement.
+
+(struct ai ())
+(struct :or2 ai (l r))
+(struct :and2 ai (l r))
+(struct :X ai (pos))
+(struct :O ai (pos))
+(struct :! ai (pos))
+
+(define (ai-eval me opp board ai-expr)
+  (let loop ([ai-expr ai-expr])
+    (match ai-expr
+      [(:or2 l r)
+       (or (loop l) (loop r))]
+      [(:and2 l r)
+       (define lv (loop l))
+       (and lv
+            (let ([rv (loop r)])
+              (and rv
+                   (if (number? rv) rv lv))))]
+      [(:X pos)
+       (eq? (vector-ref board pos) opp)]
+      [(:O pos)
+       (eq? (vector-ref board pos) me)]
+      [(:! pos)
+       (and (not (vector-ref board pos)) pos)])))
+
+(define (binary->nary add)
+  (λ args
+    (define (loop args)
+      (match args
+        [(list one) one]
+        [(cons one more)
+         (add one (loop more))]))
+    (loop args)))
+
+(define :or (binary->nary :or2))
+(define :and (binary->nary :and2))
+(define (:check :X x y z)
+  (:or (:and (:! x) (:X y) (:X z))
+       (:and (:X x) (:! y) (:X z))
+       (:and (:X x) (:X y) (:! z))))
+
+(define (:block-row s) (:block (+ s 0) (+ s 1) (+ s 2)))
+(define (:block-col s) (:block (+ s 0) (+ s 3) (+ s 6)))
+(define (:block x y z) (:check :X x y z))
+(define (:win-row s) (:win (+ s 0) (+ s 1) (+ s 2)))
+(define (:win-col s) (:win (+ s 0) (+ s 3) (+ s 6)))
+(define (:win x y z) (:check :O x y z))
+(define (:or-if x y z)
+  (:and (:X x) (:or (:! y) (:! z))))
+(define (:opposite-corner x y)
+  (:or (:and (:X x) (:! y))
+       (:and (:! x) (:X y))))
+(define optimal-ai
+  (:or
+   ;; Win
+   (:win-row 0) (:win-row 3) (:win-row 6)
+   (:win-col 0) (:win-col 1) (:win-col 2)
+   (:win 0 4 8) (:win 6 4 2)
+   ;; Block
+   (:block-row 0) (:block-row 3) (:block-row 6)
+   (:block-col 0) (:block-col 1) (:block-col 2)
+   (:block 0 4 8) (:block 6 4 2)
+   ;; XXX Fork
+   ;; XXX Block fork
+   ;; Center
+   (:! 4)
+   ;; Opposite corner
+   (:opposite-corner 0 8) (:opposite-corner 2 6)
+   ;; Empty Corner
+   (:! 0) (:! 2) (:! 6) (:! 8)
+   ;; Empty Side
+   (:! 1) (:! 3) (:! 5) (:! 7)))
+
+(define (tic-tac-toe-tree-ai ai-expr)
+  (λ (who-am-i)
+    (define opp (match who-am-i ['X 'O] ['O 'X]))
+    (λ (board)
+      (cons who-am-i (ai-eval who-am-i opp board ai-expr)))))
+
+(define (tic-tac-toe-script script)
+  (λ (who-am-i)
+    (λ (board)
+      (define next (first script))
+      (set! script (rest script))
+      (cons who-am-i next))))
+
+(module+ test
+  #;(tic-tac-toe (tic-tac-toe-tree-ai optimal-ai)
+                 (tic-tac-toe-tree-ai optimal-ai)
+                 #:display? #t)
+  #;(tic-tac-toe tic-tac-toe-interactive
+                 (tic-tac-toe-tree-ai optimal-ai))
+  (begin (tic-tac-toe (tic-tac-toe-script
+                       '(2 6 8 5)
+                       ;; Tested
+                       #;'(4 8 5 2)
+                       #;'(4 8 2 6)
+                       #;'(4 6 7 1))
+                      (tic-tac-toe-tree-ai optimal-ai)
+                      #:display? #t)
+         (exit 1)))
+
+;; Simple Neural Network AI
 
 (define (vecmat-mult! m p A B C)
   (unless (= (flvector-length A) m)
@@ -119,18 +234,6 @@
           (values i x)
           (values spot spot-score))))))
 
-(define equal-ai-matrix
-  (make-flvector (* 18 9) (/ 1.0 18.0)))
-(define (random-ai-matrix-cell)
-  (define prng (current-pseudo-random-generator))
-  (fl- (fl* 2.0 (flrandom prng)) 1.0))
-
-(define (random-ai-matrix)
-  (define AI (make-flvector (* 18 9) 0.0))
-  (for ([i (in-naturals)] [x (in-flvector AI)])
-    (flvector-set! AI i (random-ai-matrix-cell)))
-  AI)
-
 (define (random-but-not used total)
   (define next (random total))
   (if (= next used)
@@ -149,9 +252,7 @@
     (for/fold ([population
                 (build-list population-size (λ (_) (spontaneous-generation)))])
               ([gen (in-range how-many-generations)])
-      (eprintf "Generation ~a\n" gen)
-      (eprintf "\tFitness...\n")
-      (define-values (fit unfit) (time (fitness retain-count population)))
+      (define-values (fit unfit) (fitness retain-count population))
       (define surviving-unfit (take (shuffle unfit) survive-count))
       (define-values (will-mutate wont-mutate)
         (split-at surviving-unfit mutate-count))
@@ -168,6 +269,19 @@
   (define-values (best others) (fitness 1 final-population))
   (first best))
 
+(define (lerp x y %)
+  (fl+ x (fl* % (fl- y x))))
+
+(define (random-ai-matrix-cell)
+  (define prng (current-pseudo-random-generator))
+  (flrandom prng))
+
+(define (random-ai-matrix)
+  (define AI (make-flvector (* 18 9) 0.0))
+  (for ([i (in-naturals)] [x (in-flvector AI)])
+    (flvector-set! AI i (random-ai-matrix-cell)))
+  AI)
+
 (define (mutate-ai-matrix! B)
   (define prng (current-pseudo-random-generator))
   (for ([i (in-range (random 10))])
@@ -176,9 +290,6 @@
     (flvector-set! B i (fl* old (fl- (fl* 0.5 (flrandom prng)) 0.5))))
   B)
 
-(define (lerp x y %)
-  (fl+ x (fl* % (fl- y x))))
-
 (define (ai-matrix-breed B1 B2)
   (define prng (current-pseudo-random-generator))
   (define B3 (make-flvector (* 18 9)))
@@ -186,40 +297,50 @@
     (flvector-set! B3 i (lerp m f (flrandom prng))))
   B3)
 
-(define (tic-tac-toe/score P1d P2d)
+;; P1 loses to P2 if does not tie or if does not win both games
+(define (tic-tac-toe<=? P1d P2d)
   (define P1 (tic-tac-toe-matrix-ai P1d))
   (define P2 (tic-tac-toe-matrix-ai P2d))
   (match (cons (tic-tac-toe P1 P2) (tic-tac-toe P2 P1))
-    [(cons 'O 'X) 20]
-    [(cons 'Tie 'Tie) 10]
-    [(or (cons 'O 'O)
-         (cons 'X 'X))
-     5]
-    [_ 0]))
-(define ((tournament game) how-many players)
-  ;; XXX change to partial sort
-  (define players-with-scores
-    (let loop ([previous-players '()]
-               [players players])
-      (match players
-        ['() '()]
-        [(cons P1 future-players)
-         (define score
-           (+ (for/sum ([p (in-list previous-players)]) (game P1 p))
-              (for/sum ([p (in-list future-players)]) (game P1 p))))
-         (cons (cons P1 score)
-               (loop (cons P1 previous-players) future-players))])))
-  (define p*c-decreasing (sort players-with-scores >= #:key cdr))
-  (split-at (map car p*c-decreasing) how-many))
+    [(or (cons 'O 'X)
+         (cons 'Tie 'Tie))
+     #f]
+    [_ #t]))
 
-#;(module+ test
-  (define evolved-ai-matrix
-    (evolve 100 10
-            0.2 0.05 0.01
-            random-ai-matrix
-            (tournament tic-tac-toe/score)
-            mutate-ai-matrix! ai-matrix-breed))
-  evolved-ai-matrix
+(define ((tournament game<=?) how-many players)
+  (define h (make-heap game<=?))
+  (define-values (first-k-players rest-players) (split-at players how-many))
+  (for ([p (in-list first-k-players)])
+    (heap-add! h p))
+  (define left-overs '())
+  (for ([p (in-list rest-players)])
+    (heap-add! h p)
+    (set! left-overs (cons (heap-min h) left-overs))
+    (heap-remove-min! h))
+  (values (vector->list (heap->vector h))
+          left-overs))
 
-  (tic-tac-toe (tic-tac-toe-matrix-ai evolved-ai-matrix)
-               tic-tac-toe-interactive))
+(module+ test
+  (define (test-evolve)
+    (define evolved-ai-matrix
+      (evolve 128 50
+              0.2 0.10 0.10
+              random-ai-matrix
+              (tournament tic-tac-toe<=?)
+              mutate-ai-matrix! ai-matrix-breed))
+    (match
+        (tic-tac-toe (tic-tac-toe-matrix-ai evolved-ai-matrix)
+                     (tic-tac-toe-tree-ai optimal-ai))
+      ['O
+       (newline)
+       (tic-tac-toe (tic-tac-toe-matrix-ai evolved-ai-matrix)
+                    (tic-tac-toe-tree-ai optimal-ai)
+                    #:display? #t)
+       (exit 1)]
+      ['Tie 'S]
+      ['X 'F]))
+
+  (for/fold ([r (hasheq)]) ([i (in-range 80)])
+    (define e (test-evolve))
+    (display e) (flush-output)
+    (hash-update r e add1 0)))
